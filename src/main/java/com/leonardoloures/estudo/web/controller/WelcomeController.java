6cdd8f8e5a7050feca3b62ff1157402ca9b9e0d9
leonardoquirino <leonardo.lqs@gmail.com>
2018-11-06 11:15:03 -0200

    Initial commit

---
+++ src/main/java/com/leonardoloures/estudo/web/controller/WelcomeController.java
@@ -0,0 +1,32 @@
+package com.leonardoloures.estudo.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+@Controller
+public class WelcomeController {
+
+    private final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
+    @GetMapping("/")
+    public String index(Model model) {
+        logger.debug("Welcome to mkyong.com...");
+        model.addAttribute("msg", getMessage());
+        model.addAttribute("today", new Date());
+        model.addAttribute("time", LocalTime.now());
+
+        return "index";
+
+    }
+
+    private String getMessage() {
+        return "Hello World";
+    }
+
+}
